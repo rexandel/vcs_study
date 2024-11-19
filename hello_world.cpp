501974620c329df5981fc7971c13751e084d7e84
Alexander Sidorenko <xarendel@gmail.com>
2024-11-19 12:02:30 +0300

    Add a comment describing the program functionality

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 
+// This program asks the user for their name and greets them with a personalized message.
 int main() {
     std::string name;
     std::cout << "Enter your name: ";
