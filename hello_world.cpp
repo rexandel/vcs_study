dc716dabd1bb958f17417aa1c7d9636b739f50d3
Alexander Sidorenko <xarendel@gmail.com>
2024-11-19 12:33:51 +0300

    Apply Mozilla code style using clang-format

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
 #include <string>
 
-// Prompts the user to input their name and then displays a personalized greeting message.
+<<<<<<< HEAD
+// This program asks the user for their name and greets them with a personalized
+// message.
 int main() {
     std::string name;
     std::cout << "Enter your name: ";
     std::cin >> name;
     std::cout << "Hello, world from " << name << "!" << std::endl;
     return 0;
+=======
+// This program asks the user for their name and greets them with a personalized
+// message.
+int
+main()
+{
+  std::string name;
+  std::cout << "Enter your name: ";
+  std::cin >> name;
+  std::cout << "Hello, world from " << name << "!" << std::endl;
+  return 0;
+>>>>>>> b1b5b27 (Apply Mozilla code style using clang-format)
 }
